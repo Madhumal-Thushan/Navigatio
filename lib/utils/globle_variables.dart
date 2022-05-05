8df94f114d7134b07779f8a7d286632673e4120d
Madhumal-Thushan <mthushan99@gmail.com>
2022-05-06 02:44:29 +0530

    search screen

--- lib/utils/globle_variables.dart
+++ lib/utils/globle_variables.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 
 import '../screens/add_post_screen.dart';
 import '../screens/feed_screen.dart';
+import '../screens/search_screen.dart';
 
 const webScreenSize = 600;
 
 const HomeScreenItems = [
   FeedScreen(),
-  Text("Search"),
+  SearchScreen(),
   AddPostScreen(),
   Text("Notifi"),
   Text("Profile"),
